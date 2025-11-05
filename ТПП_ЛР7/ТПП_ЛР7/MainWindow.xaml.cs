e2177646725af97512711f2992bc6146d3b11b15
Рамат Гайнутдинов <157987285+Qweby@users.noreply.github.com>
2025-11-05 11:34:28 +0300

    Add files via upload

---
+++ ТПП_ЛР7/ТПП_ЛР7/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ТПП_ЛР7
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
